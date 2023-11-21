836d68a9c559db937628933df307d57d9d5344a1
amirhussein27 <amir.eshraghi77@gmail.com>
2023-11-21 13:56:47 +0330

    pull

--- src/script.js
+++ src/script.js
@@ -7,54 +7,65 @@ let myul=document.getElementById("myul")
 function addnewdo(newdo){
 // console.log(trash);
 
-console.log(newdo);
-let newdoli=document.createElement('li')
-newdoli.id="newli"
-let trash1=document.createElement('li')
-// console.log(newdotrash);
-
-trash1.className='fa fa-trash'
-trash1.id="trash"
-console.log(trash1);
-
-newdoli.innerHTML=newdo
-container.append()
-
-myul.append(newdoli,trash1)
-
-
-trash1.addEventListener('click',function(){
-    newdoli.remove()
-    trash1.remove()
-})
-deleteme.addEventListener('click',function(){
-    newdoli.remove()
-    trash1.remove()
-   })
+if(myinp.value==""){
+    alert("emptytext")
 }
+else{console.log(newdo);
+    let newdoli=document.createElement('li')
+    newdoli.id="newli"
+    let trash1=document.createElement('li')
+    // console.log(newdotrash);
+    let newhr=document.createElement('hr')
+    
+    
+    trash1.className='fa fa-remove'
+    trash1.id="trash"
+    console.log(trash1);
+    
+    newdoli.innerHTML=newdo
+    container.append()
+    
+    myul.append(newdoli,trash1,newhr)
+    
+    
+    trash1.addEventListener('click',function(){
+        newdoli.remove()
+        trash1.remove()
+        newhr.remove()
+    
+    })
+    deleteme.addEventListener('click',function(){
+        newdoli.remove()
+        trash1.remove()
+        newhr.remove()
+       })
+       newdoli.addEventListener('click',function(){
+        if( newdoli.style.textDecoration!="line-through"){  
+              newdoli.style.textDecoration="line-through"
+              newdoli.style.color="lightgreen"
+    }
+    else{
+        newdoli.style.textDecoration="none"
+        newdoli.style.color="black"
+    }
+    
+       }
+       
+       )
+    }
 
-myinp.addEventListener("keydown",function(event){
-let newdo=event.target.value
+   }
+
+addme.addEventListener('click',function(event){
+let newdo=myinp.value
     
 
     // console.log("object");
 
     // console.log(event.keyCode);
 
-    if(event.keyCode==13){
-        // console.log("13");
 
-        // console.log(event.target);
-    
-// console.log(event.target.value);
 addnewdo(newdo)
 
-        }
-   
-    else{
-        console.log("14");
-
-    }
-})
-
-
+        })
+  
